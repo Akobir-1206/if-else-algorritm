b1c002a99f8c846913725f0ea5159b07bd62f326
Akobir-1206 <akobiryuldashaliyev9@gmail.com>
2024-04-02 19:38:19 +0500

    add

---
+++ node.js
@@ -0,0 +1,103 @@
+// **********************1-misol***********
+// let x = prompt("Yoshni kirit")
+// if(x<20){
+//     console.log("Xush kelibsan")
+// }
+// else{console.log("Hali yoshsan")}
+
+// **************2-misol**************
+// let a = prompt("a ni kiriting")
+// let b = prompt("b ni kiriting")
+// let c = prompt("c ni kiriting")
+// if( (a+b)>c &&(b+c)>a&& (a+c)>b ) {
+//     console.log("bu uchburchak")
+// }
+// else{
+//     console.log("bu uchburchak emas")
+// }
+
+// *************3-misol*************
+// let son1 = prompt("1 sonni kiriting")
+// let amal = prompt("amalni kiriting")
+// let son2 = prompt("2 sonni kiritng")
+// if(amal == "/"){
+//     console.log(son1 / son2)
+// }
+// else if(amal == "*"){
+//     console.log(son1 * son2)
+// }
+// else{
+//     console.log("Error")
+// }
+
+// *************4-misol************
+
+// let x = prompt("sonni kiritng")
+// if(x>0){
+//     console.log("Bu son musbat "+ Math.round(Math.sqrt(x)))
+// }
+// else if(x<0){
+//     console.log("Bu son manfiy")
+// }
+// else{
+//     console.log(Error)
+// }
+
+// ************5-misol**************
+// let x = prompt("sonni kiritng")
+// if(x>0){
+//     console.log( (Math.sqrt(x)))
+// }
+// else{
+//     console.log("Bu sondan ildiz ciqmaydi")
+// }
+
+// **************7-misol************
+// let son = prompt("Sonni kiriting")
+// if(son % 3 === 0 && son % 5 === 0){
+//     console.log("FizzBuzz")
+// }
+// else if(son % 3 === 0){
+//     console.log("Fizz")
+// }
+// else if(son % 5 === 0){
+//     console.log("Buzz")
+// }
+ 
+// else{
+//     console.log("Adashdingiz")
+// }
+
+// **************8-misol*************
+// let son1 = prompt("1 sonni kiritng")
+// let son2 = prompt("2 sonni kiritng")
+// if(son1 % son2 ===0){
+//     console.log( son1 / son2)
+// }
+// else{
+//     console.log("Bolinmaydi")
+// }
+
+// **************9-misol*******
+// let son1 = prompt("1 sonni kiriting")
+// let son2 = prompt("2 sonni kiriting")
+// let son3 = prompt("3 sonni kiriting")
+// let arifmetik = ((son1 + son2 + son3)/3)
+
+// if((son1 + son2 + son3)%3){
+//     console.log((son1 + son2 + son3)/3)
+// }
+// else{
+//     console.log(Math.floor((son1 + son2 + son3)%3))
+// }
+
+// *****************10-misol*********
+
+// let son = prompt("Sonni kiriting")
+// if(son % 2===0){
+//     console.log(son)
+// }
+// else{
+//     console.log("Bu toq son")
+// }
+
